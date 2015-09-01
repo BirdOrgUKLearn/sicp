4374578015b23c73773896729bc54a2bbbc63e57
Pascal Hartig <phartig@twitter.com>
2015-09-01 22:51:41 +0200

    Add fact-iter example

--- chapter1/factorial.rkt
+++ chapter1/factorial.rkt
@@ -6,4 +6,14 @@
       (* n (factorial (- n 1)))))
 
 
-(factorial 5)
+(define (factorial2 n)
+  (fact-iter 1 1 n))
+
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+                 (+ counter 1)
+                 max-count)))
+
+(factorial2 5)
