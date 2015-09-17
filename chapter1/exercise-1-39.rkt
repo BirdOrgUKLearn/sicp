6b815163755cfe3bb7b22249499c09ed830b9bd0
stefan <stefan@twitter.com>
2015-09-17 20:16:13 +0100

    Fix exercise-1-39

--- chapter1/exercise-1-39.rkt
+++ chapter1/exercise-1-39.rkt
@@ -1,12 +1,10 @@
 #lang planet neil/sicp
 
-; does not work
-
 (define (cont-frac n d k)
   (define (iter i sum)
     (if (= i 0)
         sum
-        (iter (dec i) (/ (n k) (+ (d k) sum)))))
+        (iter (dec i) (/ (n i) (+ (d i) sum)))))
   (iter k 0))
 
 (define (tan-cf x k)
@@ -15,6 +13,6 @@
              k))
 ; x is radians
 (tan-cf 1 100) ; target 1.5574
-(tan-cf 0.5 1000) ; target 0.54630249
-(tan-cf 0.25 1000) ; target 0.25534192
-(tan-cf 0 1000) ; 0
+(tan-cf 0.5 100) ; target 0.54630249
+(tan-cf 0.25 100) ; target 0.25534192
+(tan-cf 0 100) ; 0
